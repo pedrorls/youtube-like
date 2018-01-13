27ede977ebbbef5dbf8b5b025452a40bd002ae82
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2018-01-13 14:41:34 -0200

    video could be selected on thumbnail

---
+++ src/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import VideosListItem from './video_list_details';
+
+const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return(
+            <VideosListItem
+                onVideoSelect={props.onVideoSelect}
+                key={ video.etag } 
+                video={ video } 
+            />
+        );
+    })
+    return(
+        <ul className='col-md-4 list-group'>
+            { videoItems }
+        </ul>
+    );
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDgI9i5rVNouZsF50yvmQzjDxZFnoPMxt0';
 
@@ -9,15 +11,20 @@ const API_KEY = 'AIzaSyDgI9i5rVNouZsF50yvmQzjDxZFnoPMxt0';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = { videos: [], selectedVideo: null };
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({ videos: videos, selectedVideo: videos[0] });
         });
     }
     render(){
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos }
+                 />
             </div>
         );
     }
